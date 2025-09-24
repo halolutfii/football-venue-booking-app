bb3a112c98f8abb00d2f2df84236f7b4b93d0e20
kuntowck <kunto312002@gmail.com>
2025-09-24 08:43:03 +0700

    [update]: file build grade change ndk version to 27

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.football_venue_booking_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
